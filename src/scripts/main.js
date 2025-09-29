977920912e751049150f74201f541767fa623cb0
Nadiia Shishova <nadin71983@gmail.com>
2025-09-29 15:41:52 +0300

    add task solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -18,10 +18,32 @@ const food = {
   },
 };
 
-const tree = document.querySelector('#tree');
+const treeContainer = document.querySelector('#tree');
 
-function createTree(element, data) {
-  // WRITE YOUR CODE HERE
+function createTreeRecursive(container, obj) {
+  if (!obj || typeof obj !== 'object') {
+    return;
+  }
+
+  const ul = document.createElement('ul');
+
+  for (const key in obj) {
+    if (!obj.hasOwnProperty(key)) {
+      continue;
+    }
+
+    const li = document.createElement('li');
+
+    li.textContent = key;
+
+    if (typeof obj[key] === 'object' && Object.keys(obj[key]).length > 0) {
+      createTreeRecursive(li, obj[key]);
+    }
+
+    ul.appendChild(li);
+  }
+
+  container.appendChild(ul);
 }
 
-createTree(tree, food);
+createTreeRecursive(treeContainer, food);
